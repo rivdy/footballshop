f44f258da0425c59f9c96ea3d39b8542e32b2492
RPR <rivaldy.rivly@gmail.com>
2025-09-16 13:33:43 +0700

    Tugas 3: Implementasi Form dan Data Delivery pada Django

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,16 @@
 from django.urls import path
-from .views import show_home
+from . import views
 
 app_name = "main"
-urlpatterns = [ path("", show_home, name="show_home") ]
+
+urlpatterns = [
+    path("", views.show_products, name="product_list"),
+    path("add/", views.create_product, name="product_add"),
+    path("product/<int:pk>/", views.product_detail, name="product_detail"),
+
+    # data delivery
+    path("json/", views.show_json, name="json"),
+    path("xml/", views.show_xml, name="xml"),
+    path("json/<int:pk>/", views.show_json_by_id, name="json_by_id"),
+    path("xml/<int:pk>/", views.show_xml_by_id, name="xml_by_id"),
+]
