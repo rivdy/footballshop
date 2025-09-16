3aa2fb12e8965c92d0891290e25aaeef04c8ef28
RPR <rivaldy.rivly@gmail.com>
2025-09-16 20:25:04 +0700

    Enable humanize and load intcomma in templates

--- footballshop/settings.py
+++ footballshop/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'main',
 ]
 
@@ -68,6 +69,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'builtins': ['django.templatetags.humanize'],
             ],
         },
     },
