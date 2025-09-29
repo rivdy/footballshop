bf30e0a75e21a1943f3bcee6e6ffc18954911038
RPR <rivaldy.rivly@gmail.com>
2025-09-29 23:00:56 +0700

    Pelengkap

--- main/forms.py
+++ main/forms.py
@@ -5,17 +5,7 @@ from main.models import Product
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
-class RegisterForm(UserCreationForm):
-    def clean_username(self):
-        username = self.cleaned_data["username"].strip()
-        if User.objects.filter(username__iexact=username).exists():
-            raise forms.ValidationError("Username sudah terpakai.")
-        return username
 
-    class Meta:
-        model = User
-        fields = ("username", "password1", "password2")
-        
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
